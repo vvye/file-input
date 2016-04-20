cb4167e11d697e0623201ad6784b35f821f00d4e
vvye <ekaiser.hellwege@gmail.com>
2016-04-20 22:00:14 +0200

    Initial commit

---
+++ fileInput.js
@@ -0,0 +1,34 @@
+function fileInput(className, options) {
+    options = options || {};
+
+    var inputs = document.getElementsByClassName(className);
+    for (var i = 0; i < inputs.length; i++) {
+        var input = inputs[i];
+        input.id = input.id || input.name || 'input-' + i;
+
+        var label = document.createElement('label');
+        label.className = input.className;
+        input.className = '';
+
+        var browseButton = document.createElement('a');
+        browseButton.innerHTML = options.buttonText || 'Browse&hellip;';
+
+        var filePathBox = document.createElement('span');
+        filePathBox.innerHTML = input.value || options.noFileText || 'No file selected.';
+
+        (function (target) {
+            input.onchange = function () {
+                target.innerHTML = this.value;
+            }
+        })(filePathBox);
+
+        label.appendChild(browseButton);
+        label.appendChild(filePathBox);
+
+        input.style.position = 'absolute';
+        input.style.visibility = 'hidden';
+
+        input.parentNode.replaceChild(label, input);
+        label.appendChild(input);
+    }
+}
