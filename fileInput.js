cf73ba5df4a839b12a749a8884ce50da3bd8216b
vvye <ekaiser.hellwege@gmail.com>
2016-04-20 22:32:28 +0200

    made only file input elements be affected

--- fileInput.js
+++ fileInput.js
@@ -4,6 +4,10 @@ function fileInput(className, options) {
     var inputs = document.getElementsByClassName(className);
     for (var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
+        if (input.tagName !== 'input' || input.type !== 'file') {
+            continue;
+        }
+        
         input.id = input.id || input.name || 'input-' + i;
 
         var label = document.createElement('label');
