5050e158fdd93ee445c512644e40c9548c299381
vvye <ekaiser.hellwege@gmail.com>
2016-04-22 19:28:19 +0200

    improved readability

--- fileInput.js
+++ fileInput.js
@@ -1,9 +1,14 @@
 function fileInput(className, options) {
     options = options || {};
+    var buttonText = options.buttonText || 'Browse&hellip;';
+    var noFileText = options.noFileText || 'No file selected.';
+    var multiFilesText = options.multiFilesText || '{num} files selected.';
+    var noFilesText = options.noFilesText || 'No files selected.';
 
     var inputs = document.getElementsByClassName(className);
     for (var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
+
         if (input.tagName.toLowerCase() !== 'input' || input.type.toLowerCase() !== 'file') {
             continue;
         }
@@ -16,20 +21,22 @@ function fileInput(className, options) {
         input.removeAttribute('class');
 
         var browseButton = document.createElement('a');
-        browseButton.innerHTML = options.buttonText || 'Browse&hellip;';
+        browseButton.innerHTML = buttonText;
 
         var fileNameBox = document.createElement('span');
-        (function (fileNameBox, input) {
+        
+        (function updateFileName(fileNameBox, input) {
             (input.onchange = function () {
                 var multiple = input.hasAttribute('multiple');
-                if ((multiple && input.files.length > 1)) {
-                    fileNameBox.innerHTML = input.files.length + ' ' + (options.multiFilesText || 'files selected.');
+                var numFiles = input.files.length;
+                if ((multiple && numFiles > 1)) {
+                    fileNameBox.innerHTML = multiFilesText.replace('{num}', '' + numFiles);
                 } else {
                     var fileName = (input.value || '').split(/[\\/]/).pop();
                     if (multiple) {
-                        fileNameBox.innerHTML = fileName || options.noFilesText || 'No files selected.';
+                        fileNameBox.innerHTML = fileName || noFilesText;
                     } else {
-                        fileNameBox.innerHTML = fileName || options.noFileText || 'No file selected.';
+                        fileNameBox.innerHTML = fileName || noFileText;
                     }
                 }
             })();
